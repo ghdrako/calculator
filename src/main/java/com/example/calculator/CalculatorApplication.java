628c47c6ea2c5582d354d86f775de8c0acdcb6f8
akepka <akepka@o2.pl>
2022-05-14 11:13:30 +0200

    Change to pass checkstyle:check

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -2,12 +2,23 @@ package com.example.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Javadoc.
+ */
 @SpringBootApplication
 public class CalculatorApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
-	}
-
+    //private CalculatorApplication() {
+       //not called
+    //}
+    /**
+       * Method add two operands.
+       * @param  args  args
+    */
+    public static void main(final String[] args) {
+        //SpringApplication.run(CalculatorApplication.class, args);
+        new CalculatorApplication.run(args);
+    }
+    private void run(final String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
 }
